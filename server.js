17058df8b197038a741837f52fcb066521a7ff03
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2021-11-17 13:33:21 -0500

    connect to db, handle login

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require('express') //import express module
 const app = new express() //create instance of express web server
+const db = require('better-sqlite3')('login.db')
 
 //serves web files for GET on /(root)
 app.use(express.static("public"))
 
 //handle GET request on /login
 app.get("/login",(req,res) => {
-    res.json({
-        message: "Hello!"
-    })
+    const user = db.prepare("SELECT * FROM users WHERE username = ? AND password = ?")
+        .get(req.body.username, req.body.password)
+    if (user) { //if user is defined, truthy
+        //res.status(200) //this is default
+        res.json({
+            message: "Hello!",
+            userId: user.id
+        })
+    }
+    else {
+        res.status(401);
+        res.json({
+            message: "No user found.",
+        })
+    }
 })
 
 app.post("/register",(req,res)=>{
